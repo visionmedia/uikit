4670b3ee583de063ea235a3e0debc294551c8813
TJ Holowaychuk <tj@vision-media.ca>
2012-02-02 21:24:35 -0800

    build

--- build/ui.js
+++ build/ui.js
@@ -23,7 +23,8 @@ function Emitter() {
  *
  * @param {String} event
  * @param {Function} fn
- * @return {Emitter} for chaining
+ * @return {Emitter}
+ * @api public
  */
 
 Emitter.prototype.on = function(event, fn){
@@ -33,11 +34,59 @@ Emitter.prototype.on = function(event, fn){
 };
 
 /**
+ * Adds an `event` listener that will be invoked a single
+ * time then automatically removed.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.once = function(event, fn){
+  var self = this;
+
+  function on() {
+    self.off(event, on);
+    fn.apply(this, arguments);
+  }
+
+  this.on(event, on);
+  return this;
+};
+
+/**
+ * Remove the given callback for `event` or all
+ * registered callbacks.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.off = function(event, fn){
+  var callbacks = this.callbacks[event];
+  if (!callbacks) return this;
+
+  // remove all handlers
+  if (1 == arguments.length) {
+    delete this.callbacks[event];
+    return this;
+  }
+
+  // remove specific handler
+  var i = callbacks.indexOf(fn);
+  callbacks.splice(i, 1);
+  return this;
+};
+
+/**
  * Emit `event` with the given args.
  *
  * @param {String} event
  * @param {Mixed} ...
- * @return {Emitter} for chaining
+ * @return {Emitter} 
  */
 
 Emitter.prototype.emit = function(event){
